bb0c0d78e7f9ef309cfc4ca2b50cc2a448291bd8
Marwan-98 <marwanashrafzaki98@gmail.com>
2022-11-03 19:50:14 +0200

    Loading Update

--- pages/signUp.tsx
+++ pages/signUp.tsx
@@ -10,10 +10,13 @@ import axios from "axios";
 import { useRouter } from "next/router";
 import Link from "next/link";
 import { useSupabaseClient } from "@supabase/auth-helpers-react";
+import { motion } from "framer-motion";
+import { useState } from "react";
 
 const SignUp = () => {
   const router = useRouter();
   const supabase = useSupabaseClient();
+  const [loading, setLoading] = useState(false);
 
   const formik = useFormik({
     initialValues: {
@@ -24,45 +27,57 @@ const SignUp = () => {
       password: "",
     },
     onSubmit: async (values) => {
+      setLoading(true);
       try {
         const { data, error } = await supabase.auth.signUp({
           email: values.email,
           password: values.password,
         });
         if (error) throw error;
-        if (data)
-          axios.put("/api/addUser", { values }).then((res) => router.push("/"));
+        if (data) {
+          axios.put("/api/addUser", { values }).then((res) => {
+            router.push("/");
+            setLoading(false);
+          });
+        }
       } catch (error) {
         console.log(error);
+        setLoading(false);
       }
     },
   });
   return (
-    <>
-      <div className="flex min-h-full">
-        <div className="flex flex-1 flex-col justify-center py-12 px-4 sm:px-6 lg:flex-none lg:px-20 xl:px-24">
-          <div className="mx-auto w-full max-w-sm lg:w-96">
-            <div>
-              <img
-                className="h-7 w-auto"
-                src="./logo-3.png"
-                alt="Your Company"
-              />
-              <h2 className="mt-6 text-3xl font-bold tracking-tight text-gray-900">
-                Sign up to your account
-              </h2>
-              <p className="mt-2 text-sm text-gray-600">
-                Already Have an Account?{" "}
-                <Link href="/signIn">
-                  <span className="font-medium text-indigo-600 hover:text-indigo-500 cursor-pointer">
-                    Sign In
-                  </span>
-                </Link>
-              </p>
-            </div>
+    <div className="h-screen">
+      {!loading ? (
+        <motion.div
+          className="flex min-h-full"
+          exit={{
+            x: 2000,
+          }}
+        >
+          <div className="flex flex-1 flex-col justify-center py-12 px-4 sm:px-6 lg:flex-none lg:px-20 xl:px-24">
+            <div className="mx-auto w-full max-w-sm lg:w-96">
+              <div>
+                <img
+                  className="h-7 w-auto"
+                  src="./logo-3.png"
+                  alt="Your Company"
+                />
+                <h2 className="mt-6 text-3xl font-bold tracking-tight text-gray-900">
+                  Sign up to your account
+                </h2>
+                <p className="mt-2 text-sm text-gray-600">
+                  Already Have an Account?{" "}
+                  <Link href="/signIn">
+                    <span className="font-medium text-indigo-600 hover:text-indigo-500 cursor-pointer">
+                      Sign In
+                    </span>
+                  </Link>
+                </p>
+              </div>
 
-            <div className="mt-8">
-              {/* <div>
+              <div className="mt-8">
+                {/* <div>
                 <div>
                   <p className="text-sm font-medium text-gray-700">
                     sign up with
@@ -107,120 +122,120 @@ const SignUp = () => {
                 </div>
               </div> */}
 
-              <div className="mt-6">
-                <form
-                  action="#"
-                  method="POST"
-                  className="space-y-6"
-                  onSubmit={formik.handleSubmit}
-                >
-                  <div>
-                    <label
-                      htmlFor="name"
-                      className="block text-sm font-medium text-gray-700"
-                    >
-                      First Name
-                    </label>
-                    <div className="mt-1">
-                      <input
-                        id="firstName"
-                        name="firstName"
-                        type="name"
-                        autoComplete="name"
-                        required
-                        onChange={formik.handleChange}
-                        className="block w-full appearance-none rounded-md border border-gray-300 px-3 py-2 placeholder-gray-400 shadow-sm focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm"
-                      />
-                    </div>
-                  </div>
-
-                  <div>
-                    <label
-                      htmlFor="name"
-                      className="block text-sm font-medium text-gray-700"
-                    >
-                      Last Name
-                    </label>
-                    <div className="mt-1">
-                      <input
-                        id="lastName"
-                        name="lastName"
-                        type="name"
-                        autoComplete="name"
-                        onChange={formik.handleChange}
-                        required
-                        className="block w-full appearance-none rounded-md border border-gray-300 px-3 py-2 placeholder-gray-400 shadow-sm focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm"
-                      />
-                    </div>
-                  </div>
-
-                  <div>
-                    <label
-                      htmlFor="country"
-                      className="block text-sm font-medium text-gray-700"
-                    >
-                      Country
-                    </label>
-                    <div className="mt-1">
-                      <select
-                        id="country"
-                        name="country"
-                        className="mt-1 block w-full rounded-md border-gray-300 py-2 pl-3 pr-10 text-base focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm"
-                        onChange={formik.handleChange}
-                        required
+                <div className="mt-6">
+                  <form
+                    action="#"
+                    method="POST"
+                    className="space-y-6"
+                    onSubmit={formik.handleSubmit}
+                  >
+                    <div>
+                      <label
+                        htmlFor="name"
+                        className="block text-sm font-medium text-gray-700"
                       >
-                        {countries.map((country, idx) => (
-                          <option
-                            key={idx}
-                            value={`${country.countryName}, ${country.currencyCode}`}
-                          >
-                            {country.countryName} ({country.currencyCode})
-                          </option>
-                        ))}
-                      </select>
+                        First Name
+                      </label>
+                      <div className="mt-1">
+                        <input
+                          id="firstName"
+                          name="firstName"
+                          type="name"
+                          autoComplete="name"
+                          required
+                          onChange={formik.handleChange}
+                          className="block w-full appearance-none rounded-md border border-gray-300 px-3 py-2 placeholder-gray-400 shadow-sm focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm"
+                        />
+                      </div>
                     </div>
-                  </div>
 
-                  <div>
-                    <label
-                      htmlFor="email"
-                      className="block text-sm font-medium text-gray-700"
-                    >
-                      Email address
-                    </label>
-                    <div className="mt-1">
-                      <input
-                        id="email"
-                        name="email"
-                        type="email"
-                        autoComplete="email"
-                        onChange={formik.handleChange}
-                        required
-                        className="block w-full appearance-none rounded-md border border-gray-300 px-3 py-2 placeholder-gray-400 shadow-sm focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm"
-                      />
+                    <div>
+                      <label
+                        htmlFor="name"
+                        className="block text-sm font-medium text-gray-700"
+                      >
+                        Last Name
+                      </label>
+                      <div className="mt-1">
+                        <input
+                          id="lastName"
+                          name="lastName"
+                          type="name"
+                          autoComplete="name"
+                          onChange={formik.handleChange}
+                          required
+                          className="block w-full appearance-none rounded-md border border-gray-300 px-3 py-2 placeholder-gray-400 shadow-sm focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm"
+                        />
+                      </div>
                     </div>
-                  </div>
 
-                  <div className="space-y-1">
-                    <label
-                      htmlFor="password"
-                      className="block text-sm font-medium text-gray-700"
-                    >
-                      Password
-                    </label>
-                    <div className="mt-1">
-                      <input
-                        id="password"
-                        name="password"
-                        type="password"
-                        autoComplete="current-password"
-                        onChange={formik.handleChange}
-                        required
-                        className="block w-full appearance-none rounded-md border border-gray-300 px-3 py-2 placeholder-gray-400 shadow-sm focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm"
-                      />
+                    <div>
+                      <label
+                        htmlFor="country"
+                        className="block text-sm font-medium text-gray-700"
+                      >
+                        Country
+                      </label>
+                      <div className="mt-1">
+                        <select
+                          id="country"
+                          name="country"
+                          className="mt-1 block w-full rounded-md border-gray-300 py-2 pl-3 pr-10 text-base focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm"
+                          onChange={formik.handleChange}
+                          required
+                        >
+                          {countries.map((country, idx) => (
+                            <option
+                              key={idx}
+                              value={`${country.countryName}, ${country.currencyCode}`}
+                            >
+                              {country.countryName} ({country.currencyCode})
+                            </option>
+                          ))}
+                        </select>
+                      </div>
                     </div>
-                  </div>
-                  {/* 
+
+                    <div>
+                      <label
+                        htmlFor="email"
+                        className="block text-sm font-medium text-gray-700"
+                      >
+                        Email address
+                      </label>
+                      <div className="mt-1">
+                        <input
+                          id="email"
+                          name="email"
+                          type="email"
+                          autoComplete="email"
+                          onChange={formik.handleChange}
+                          required
+                          className="block w-full appearance-none rounded-md border border-gray-300 px-3 py-2 placeholder-gray-400 shadow-sm focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm"
+                        />
+                      </div>
+                    </div>
+
+                    <div className="space-y-1">
+                      <label
+                        htmlFor="password"
+                        className="block text-sm font-medium text-gray-700"
+                      >
+                        Password
+                      </label>
+                      <div className="mt-1">
+                        <input
+                          id="password"
+                          name="password"
+                          type="password"
+                          autoComplete="current-password"
+                          onChange={formik.handleChange}
+                          required
+                          className="block w-full appearance-none rounded-md border border-gray-300 px-3 py-2 placeholder-gray-400 shadow-sm focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm"
+                        />
+                      </div>
+                    </div>
+                    {/* 
                   <div className="flex items-center justify-between">
                     <div className="flex items-center">
                       <input
@@ -244,28 +259,50 @@ const SignUp = () => {
                     </div>
                   </div> */}
 
-                  <div>
-                    <button
-                      type="submit"
-                      className="flex w-full justify-center rounded-md border border-transparent bg-indigo-600 py-2 px-4 text-sm font-medium text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
-                    >
-                      sign up
-                    </button>
-                  </div>
-                </form>
+                    <div>
+                      <button
+                        type="submit"
+                        className="flex w-full justify-center rounded-md border border-transparent bg-indigo-600 py-2 px-4 text-sm font-medium text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
+                      >
+                        sign up
+                      </button>
+                    </div>
+                  </form>
+                </div>
               </div>
             </div>
           </div>
-        </div>
-        <div className="relative hidden w-0 flex-1 lg:block">
-          <img
-            className="absolute inset-0 h-full w-full object-cover"
-            src="./signup.jpg"
-            alt=""
+          <div className="relative hidden w-0 flex-1 lg:block">
+            <img
+              className="absolute inset-0 h-full w-full object-cover"
+              src="./signup.jpg"
+              alt=""
+            />
+          </div>
+        </motion.div>
+      ) : (
+        <motion.div
+          className="flex flex-col h-full w-full justify-center items-center"
+          initial={{
+            x: -2000,
+          }}
+          animate={{
+            x: 0,
+          }}
+          transition={{
+            duration: 1,
+          }}
+        >
+          <h1 className="text-6xl p-5">Setting Up Your Account...</h1>
+          <div
+            className="w-full grow bg-center bg-cover bg-no-repeat"
+            style={{
+              backgroundImage: "url('./flight.gif')",
+            }}
           />
-        </div>
-      </div>
-    </>
+        </motion.div>
+      )}
+    </div>
   );
 };
 
